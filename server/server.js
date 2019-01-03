c5639255e367711765be341c75d6b0af6d9314e9
George <georgezqian@gmail.com>
2019-01-02 20:25:02 -0800

    Major layout improvements and nav bar implementation

--- server/server.js
+++ server/server.js
@@ -7,7 +7,10 @@ const app = express();
 app.use(morgan('tiny'));
 app.use('/watches/:wid', express.static('public'));
 
+app.use('/api/watches/:wid/photos', proxy({ target: 'http://localhost:3001', changeOrigin: true }));
+app.use('/api/watches/:wid/summary', proxy({ target: 'http://localhost:3002', changeOrigin: true }));
 app.use('/api/watches/:wid/details', proxy({ target: 'http://localhost:3003', changeOrigin: true }));
+app.use('/api/watches/:wid/reviews', proxy({ target: 'http://localhost:3004', changeOrigin: true }));
 
 app.listen(3000, () => {
   console.log('listening on port 3000...');

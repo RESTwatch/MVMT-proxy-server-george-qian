a65cf367b5933f4a20412c382c724556650a7e4c
George <georgezqian@gmail.com>
2019-01-04 19:37:00 -0800

    Configure for Docker, replace localhost url's with public IP's

--- server/server.js
+++ server/server.js
@@ -8,8 +8,8 @@ app.use(morgan('tiny'));
 app.use('/watches/:wid', express.static('public'));
 
 app.use('/api/watches/:wid/photos', proxy({ target: 'http://localhost:3001', changeOrigin: true }));
-app.use('/api/watches/:wid/summary', proxy({ target: 'http://localhost:3002', changeOrigin: true }));
-app.use('/api/watches/:wid/details', proxy({ target: 'http://localhost:3003', changeOrigin: true }));
+app.use('/api/watches/:wid/summary', proxy({ target: 'http://54.87.165.253', changeOrigin: true }));
+app.use('/api/watches/:wid/details', proxy({ target: 'http://13.57.38.154', changeOrigin: true }));
 app.use('/api/watches/:wid/reviews', proxy({ target: 'http://localhost:3004', changeOrigin: true }));
 
 app.listen(3000, () => {
